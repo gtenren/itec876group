cc34f5da3c332d9c02747de05b528ad00d87f8c9
Gary Lau <ka-yu.lau@students.mq.edu.au>
2019-09-23 21:13:37 +1000

    fix from InstanceNormalization Error

--- dl-4-tsc/classifiers/encoder.py
+++ dl-4-tsc/classifiers/encoder.py
@@ -4,6 +4,8 @@ import keras_contrib
 import numpy as np
 import pandas as pd
 import time
+from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
+
 
 from utils.utils import save_logs
 
@@ -22,19 +24,19 @@ class Classifier_ENCODER:
 
         # conv block -1
         conv1 = keras.layers.Conv1D(filters=128,kernel_size=5,strides=1,padding='same')(input_layer)
-        conv1 = keras_contrib.layers.InstanceNormalization()(conv1)
+        conv1 = InstanceNormalization()(conv1)
         conv1 = keras.layers.PReLU(shared_axes=[1])(conv1)
         conv1 = keras.layers.Dropout(rate=0.2)(conv1)
         conv1 = keras.layers.MaxPooling1D(pool_size=2)(conv1)
         # conv block -2
         conv2 = keras.layers.Conv1D(filters=256,kernel_size=11,strides=1,padding='same')(conv1)
-        conv2 = keras_contrib.layers.InstanceNormalization()(conv2)
+        conv2 = InstanceNormalization()(conv2)
         conv2 = keras.layers.PReLU(shared_axes=[1])(conv2)
         conv2 = keras.layers.Dropout(rate=0.2)(conv2)
         conv2 = keras.layers.MaxPooling1D(pool_size=2)(conv2)
         # conv block -3
         conv3 = keras.layers.Conv1D(filters=512,kernel_size=21,strides=1,padding='same')(conv2)
-        conv3 = keras_contrib.layers.InstanceNormalization()(conv3)
+        conv3 = InstanceNormalization()(conv3)
         conv3 = keras.layers.PReLU(shared_axes=[1])(conv3)
         conv3 = keras.layers.Dropout(rate=0.2)(conv3)
         # split for attention
@@ -45,7 +47,7 @@ class Classifier_ENCODER:
         multiply_layer = keras.layers.Multiply()([attention_softmax,attention_data])
         # last layer
         dense_layer = keras.layers.Dense(units=256,activation='sigmoid')(multiply_layer)
-        dense_layer = keras_contrib.layers.InstanceNormalization()(dense_layer)
+        dense_layer = InstanceNormalization()(dense_layer)
         # output layer
         flatten_layer = keras.layers.Flatten()(dense_layer)
         output_layer = keras.layers.Dense(units=nb_classes,activation='softmax')(flatten_layer)
